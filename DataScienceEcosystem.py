1518d65d6ed64fc9bee59aec21c45b4548644696
Hudson Barbosa <105313249+Hudson-Hud@users.noreply.github.com>
2023-05-04 14:55:05 -0300

    DataScienceEcosystem

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook I will briefly present some to be used to aid the study in Data Science
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science.
+# - Describe the stages of the Data Science process.
+# - Identify the main Python libraries for Data Science.
+
+# I will list Some of the most popular programming languages that Data Scientists use are:
+# 
+# **1 - Python** - Preferred and used by about 83% of Data Science professionals.
+# 
+# **2 - R** - Designed for statistical computing.
+# 
+# **3 - C/C++** - Compiled, with great efficiency in data processing.
+# 
+# **4 - Java** - One of the most popular languages.
+# 
+# **5 - JavaScript** - Currently, the most used language in the world.
+# 
+# **6 - Julia** - Designed for agile implementation of mathematical concepts.
+# 
+# **7 - Scala** - Multiparadigm language.
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# 1- Pandas - Data analysis library that provides easy-to-use data structures and data analysis tools.
+# 
+# 
+# 2- Numpy- Library for scientific computing that provides support for multidimensional arrays and matrices.
+# 
+# 
+# 3- Matplotlib - Library for data visualization in Python.
+# 
+# 
+# 4- Scikit-learn - Python machine learning library.
+# 
+# 
+# 5- TensorFlow - Open source machine learning library developed by the Google Brain Team.
+# 
+# 
+# 6- Keras - Deep learning library written in Python.
+# 
+# 
+# 7- PyTorch - Deep learning library powered by Facebook AI Research.
+# 
+
+# Example of some open source tools used in Data Science
+# 
+# | Data Science Tools |
+# | --- |
+# | Apache Hadoop |
+# | Apache Spark |
+# | Python |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# - Addition: 2 + 2 = 4
+# - Subtraction: 5 - 3 = 2
+# - Multiplication: 4 * 3 = 12
+# - Division: 10 / 2 = 5
+# - Exponentiation: 2 ** 3 = 8
+# - Modulo: 10 % 3 = 1
+
+# In[1]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+(3 * 4) + 5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+200 / 60
+
+
+# ## Author
+# 
+# Hudson Barbosa de Santana
+
+# 
